2e91775880c24d4f3510babe0a088ec9a3a55285
Matan Amram <matan15455@gmail.com>
2026-05-16 14:45:19 +0300

    fix: update SERVER_URL to correct IP address in socket.js

--- utils/socket.js
+++ utils/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 let socket = null;
 
-export const SERVER_URL = 'http://10.69.2.162:5000'; // שנה ל-IP שלך
+export const SERVER_URL = 'http://192.168.7.25:5000'; // שנה ל-IP שלך
 
 export function connectSocket(token) {
   if (socket) {
