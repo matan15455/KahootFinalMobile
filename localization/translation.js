a26d257c282e6eff2a505036a03cc60cae37c051
Matan Amram <matan15455@gmail.com>
2026-07-13 01:15:29 +0300

    feat: add Spanish localization support

--- localization/translation.js
+++ localization/translation.js
@@ -2,8 +2,9 @@ import { getLocales } from 'expo-localization';
 import { I18n } from 'i18n-js';
 import he from './languages/he.json';
 import en from './languages/en.json';
+import es from './languages/es.json';
 
-const i18n = new I18n({ he, en });
+const i18n = new I18n({ he, en,es });
 i18n.locale = getLocales()[0].languageCode; // switc to 'en'
 i18n.enableFallback = true;
 i18n.defaultLocale = 'he';
