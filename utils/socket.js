fe7f5fbc4c04c536d6d90669ceb9eb5578454209
Matan Amram <matan15455@gmail.com>
2026-07-12 19:53:13 +0300

    fix: update SERVER_URL to correct address

--- utils/socket.js
+++ utils/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 let socket = null;
 
-export const SERVER_URL = 'http://192.168.7.18:5000'; 
+export const SERVER_URL = 'http://192.168.7.3:5000'; 
 
 export function connectSocket(token) {
   if (socket) {
